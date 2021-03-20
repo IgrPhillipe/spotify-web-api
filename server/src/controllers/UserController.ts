26d6950dfb5bad58d82b084ea4bd1ebf7511a2f7
igrphillipe <igrphillipe@gmail.com>
2021-03-20 20:17:54 -0300

    Feature(project-base): create api connection

--- server/src/controllers/UserController.ts
+++ server/src/controllers/UserController.ts
@@ -1,33 +1,95 @@
 import { Request, Response, NextFunction } from 'express';
+import axios from 'axios';
+import querystring from 'querystring';
 import SpotifyWebApi from 'spotify-web-api-node';
 
+const clientId = 'cf548fe37bec4a20b5103b1d6d19c076';
+const clientSecret = '0c6a064e16df40bd9788ca224cb9294f';
+const redirect_uri1 = 'http://localhost:3000/callback/';
+const redirect_uri2 = 'http://localhost:3000/callback/user';
+const scope = 'user-read-private user-read-email';
+let authorizationCode: any;
+
 class UserController {
-  async getUser(_req: Request, res: Response, next: NextFunction) {
+  async generateAccessToken(req: Request, res: Response, next: NextFunction) {
     try {
+      res.redirect(`https://accounts.spotify.com/authorize?${
+        querystring.stringify({
+          response_type: 'code',
+          client_id: clientId,
+          scope,
+          redirect_uri: redirect_uri1,
+        })}`);
 
-      const clientId = 'cf548fe37bec4a20b5103b1d6d19c076';
-      const clientSecret = '0c6a064e16df40bd9788ca224cb9294f';
-      const redirect_uri = 'http://localhost:3000/callback';
-      const scopes = 'user-read-private user-read-email';
+      return next();
+    } catch (error) {
+      return next(error);
+    }
+  }
 
-      res.redirect('https://accounts.spotify.com/authorize' +
-        '?response_type=code' +
-        '&client_id=' + clientId +
-        (scopes ? '&scope=' + encodeURIComponent(scopes) : '') +
-        '&redirect_uri=' + encodeURIComponent(redirect_uri));
-      ).try((_res) => {
+  async getAccessToken(req: Request, res: Response, next: NextFunction) {
+    try {
+      authorizationCode = req.query.code;
 
-    });
+      const authOptions = {
+        url: 'https://accounts.spotify.com/api/token',
+        form: {
+          code: authorizationCode,
+          redirect_uri: redirect_uri2,
+          grant_type: 'authorization_code',
+        },
+        Headers: {
+          Authorization: `Basic${Buffer.from(`${clientId}:${clientSecret}`).toString('base64')}`,
+        },
+        json: true,
+      };
 
-      const spotifyApi = new SpotifyWebApi({
-        clientId: 'cf548fe37bec4a20b5103b1d6d19c076',
-        clientSecret: '0c6a064e16df40bd9788ca224cb9294f',
-        redirectUri: 'http://localhost:3000/callback',
+      console.log('oi');
+      axios.post(authOptions.url, authOptions).then(async (body) => {
+        const accessToken = body.data.access_token;
+        const refreshToken = body.data.refresh_token;
+
+        const options = {
+          url: 'https://api.spotify.com/v1/me',
+          headers: { Authorization: `Bearer ${accessToken}` },
+          json: true,
+        };
+
+        await axios.get(options.url, options).then((resData) => {
+          console.log('ou');
+          console.log(resData);
+        });
       });
 
-      spotifyApi.getMe().then((data: any) => {
-        console.log(data);
+      return next();
+    } catch (error) {
+      return next(error);
+    }
+  }
+
+  async getUser(req: Request, res: Response, next: NextFunction) {
+    try {
+      const spotifyAPi = new SpotifyWebApi({
+        clientId,
+        clientSecret,
+        redirectUri: redirect_uri2,
       });
+
+      spotifyAPi
+        .authorizationCodeGrant(authorizationCode)
+        .then((data) => {
+          console.log(data);
+        });
+
+      return next();
+    } catch (error) {
+      return next(error);
+    }
+  }
+
+  async getData(req: Request, res: Response, next: NextFunction) {
+    try {
+      console.log(req);
 
       return next();
     } catch (error) {
--- server/src/routes/UserRoutes.ts
+++ server/src/routes/UserRoutes.ts
@@ -1,9 +1,70 @@
-import { Router } from 'express';
-import UserController from '../controllers/UserController';
+import {
+  Router, Request, Response, NextFunction,
+} from 'express';
+import SpotifyWebApi from 'spotify-web-api-node';
 
 const router = Router();
 
-router.route('/')
-  .get(UserController.getUser);
+const scopes = ['user-read-private', 'user-read-email', 'playlist-modify-public', 'playlist-modify-private'];
+
+const clientId = 'cf548fe37bec4a20b5103b1d6d19c076';
+const clientSecret = '0c6a064e16df40bd9788ca224cb9294f';
+const redirectUri = 'http://localhost:3000/callback';
+
+const SpotifyApi = new SpotifyWebApi({
+  clientId,
+  clientSecret,
+  redirectUri,
+});
+
+const generateRandomString = (lenght) => {
+  let text = '';
+  const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+  for (let i = 0; i < lenght; i += 1) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+
+  return text;
+};
+
+router.get('/', (req: Request, res: Response, next: NextFunction) => {
+  res.send('Index');
+
+  next();
+});
+
+router.get('/login', (req: Request, res: Response, next: NextFunction) => {
+  const state = generateRandomString(16);
+  const html = SpotifyApi.createAuthorizeURL(scopes, state);
+
+  // console.log(html);
+  // res.send(`${html}&show_dialog=true`);
+
+  res.redirect(html);
+
+  return next();
+});
+
+router.get('/callback', async (req: Request, res: Response, next: NextFunction) => {
+  const { code } = req.query;
+
+  try {
+    const data = await SpotifyApi.authorizationCodeGrant(code);
+    const { access_token, refresh_token } = data.body;
+    SpotifyApi.setAccessToken(access_token);
+    SpotifyApi.setRefreshToken(refresh_token);
+
+    SpotifyApi.getMe().then((user) => {
+      console.log(user);
+    });
+
+    res.redirect('http://localhost:3000/');
+
+    return next();
+  } catch (error) {
+    return next(error);
+  }
+});
 
 export default router;
